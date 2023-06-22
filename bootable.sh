88de33c9111293af1997228eb137e92956125a7e
ilmns <33120809+ilmns@users.noreply.github.com>
2023-06-22 21:55:58 +0300

    Update bootable.sh

--- bootable.sh
+++ bootable.sh
@@ -1,119 +1,49 @@
 #!/bin/bash
+set -e
+image=${1:?Supply the .iso image of a Gentoo installation medium}
+target=${2:?Supply the target device}
 
-# Set the target NVMe drive for installation
-TARGET_DRIVE="/dev/nvme0n1"
+echo Checking for the necessary tools presence...
+which syslinux
+which sfdisk
+which mkfs.vfat
 
-# Set the EFI partition mount point
-EFI_MOUNT="/mnt/gentoo/boot/efi"
+echo Mounting Gentoo CD image...
+cdmountpoint=/mnt/gentoo-cd
+mkdir -p "$cdmountpoint"
+trap 'echo Unmounting Gentoo CD image...; umount "$cdmountpoint"' EXIT
+mount -o loop,ro "$image" "$cdmountpoint"
 
-# Set the hostname and timezone
-HOSTNAME="gentoo"
-TIMEZONE="Europe/Helsinki"
+echo Creating a disk-wide EFI FAT partition on "$target"...
+echo ',,U,*' | sfdisk --wipe always "$target"
 
-# Set the root password
-ROOT_PASSWORD="myrootpassword"
+echo Installing syslinux MBR on "$target"...
+dd if=/usr/share/syslinux/mbr.bin of="$target"
+sleep 1
 
-echo "Step 1: Partitioning the NVMe drive"
-# Partition the NVMe drive
-parted -s "$TARGET_DRIVE" mklabel gpt
-parted -s "$TARGET_DRIVE" mkpart primary fat32 1MiB 512MiB
-parted -s "$TARGET_DRIVE" set 1 esp on
-parted -s "$TARGET_DRIVE" mkpart primary ext4 512MiB 100%
-mkfs.fat -F 32 "${TARGET_DRIVE}p1"
-mkfs.ext4 "${TARGET_DRIVE}p2"
+echo Creating file system on "$target"1...
+mkfs.vfat "$target"1 -n GENTOO
 
-echo "Step 2: Mounting the partitions"
-# Mount the partitions
-mount "${TARGET_DRIVE}p2" /mnt/gentoo
-mkdir -p "$EFI_MOUNT"
-mount "${TARGET_DRIVE}p1" "$EFI_MOUNT"
+echo Mounting file system...
+mountpoint=/mnt/gentoo-usb
+mkdir -p "$mountpoint"
+mount "$target"1 "$mountpoint"
 
-echo "Step 3: Setting the date"
-# Set the date
-ntpd -q -g
+echo Copying files...
+cp -r "$cdmountpoint"/* "$mountpoint"/
+mv "$mountpoint"/isolinux/* "$mountpoint"
+mv "$mountpoint"/isolinux.cfg "$mountpoint"/syslinux.cfg
+rm -rf "$mountpoint"/isolinux*
+mv "$mountpoint"/memtest86 "$mountpoint"/memtest
+sed -i -e "s:cdroot:cdroot slowusb:" -e "s:kernel memtest86:kernel memtest:" "$mountpoint"/syslinux.cfg
 
-echo "Step 4: Extracting the Gentoo stage3 tarball"
-# Extract the Gentoo stage3 tarball from the live USB
-LIVEUSB="/mnt/livecd"
-STAGE3_FILE=$(ls $LIVEUSB/stage3-*.tar.xz)
-tar xvf "$STAGE3_FILE" -C /mnt/gentoo --xattrs-include='*.*' --numeric-owner --exclude='*.tar.xz'
+echo Unmounting file system...
+umount "$mountpoint"
 
-echo "Step 5: Configuring the Gentoo installation"
-# Configure the Gentoo installation
-mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
-mkdir -p /mnt/gentoo/etc/portage/repos.conf
-cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
-cp -L /etc/resolv.conf /mnt/gentoo/etc/
+echo Installing syslinux on "$target"1
+syslinux "$target"1
 
-echo "Step 6: Mounting necessary filesystems"
-# Mount necessary filesystems
-mount --types proc /proc /mnt/gentoo/proc
-mount --rbind /sys /mnt/gentoo/sys
-mount --make-rslave /mnt/gentoo/sys
-mount --rbind /dev /mnt/gentoo/dev
-mount --make-rslave /mnt/gentoo/dev
+echo Syncing...
+sync
 
-echo "Step 7: Entering the chroot environment"
-# Entering the chroot environment
-chroot /mnt/gentoo /bin/bash <<EOF
-
-source /etc/profile
-export PS1="(chroot) $PS1"
-
-echo "Step 8: Setting the timezone"
-# Set the timezone
-ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
-echo "$TIMEZONE" > /etc/timezone
-
-echo "Step 9: Setting the system clock"
-# Set the system clock
-hwclock --systohc
-
-echo "Step 10: Configuring the locale"
-# Configure the locale
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-eselect locale set en_US.utf8
-
-echo "Step 11: Setting the hostname"
-# Set the hostname
-echo "hostname=\"$HOSTNAME\"" > /etc/conf.d/hostname
-
-echo "Step 12: Setting the root password"
-# Set the root password
-echo "root:$ROOT_PASSWORD" | chpasswd
-
-echo "Step 13: Installing the Gentoo base system"
-# Install the Gentoo base system
-emerge-webrsync
-emerge --sync
-emerge --oneshot sys-apps/portage
-emerge --update --deep --newuse @world
-
-echo "Step 14: Configuring the bootloader (GRUB)"
-# Configure the bootloader (GRUB)
-emerge sys-boot/grub:2
-grub-install --target=x86_64-efi --efi-directory="$EFI_MOUNT" --bootloader-id=Gentoo
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo "Step 15: Enabling necessary services"
-# Enable necessary services
-rc-update add sshd default
-rc-update add dhcpcd default
-
-EOF
-
-echo "Step 16: Generating fstab"
-# Generate fstab
-genfstab -U /mnt/gentoo >> /mnt/gentoo/etc/fstab
-
-echo "Step 17: Exiting the chroot environment"
-# Exiting the chroot environment
-umount /mnt/gentoo/proc
-umount /mnt/gentoo/sys
-umount /mnt/gentoo/dev{/pts,/shm}
-umount /mnt/gentoo{/boot/efi,/sys,/dev}
-
-umount /mnt/gentoo
-
-echo "Installation completed. You can now reboot your system."
+echo 'Done!'
