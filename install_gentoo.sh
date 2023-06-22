ce0f9ca7a94b951891f94d77382f8486f453aa2f
ilmns <33120809+ilmns@users.noreply.github.com>
2023-06-22 21:20:35 +0300

    Update install_gentoo.sh

--- install_gentoo.sh
+++ install_gentoo.sh
@@ -37,7 +37,7 @@ echo "Step 4: Downloading the Gentoo stage3 tarball"
 STAGE3_URL="http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64.txt"
 STAGE3_FILE=$(wget -qO- "$STAGE3_URL" | grep -Eo 'stage3-amd64-.*\.tar\.xz' | tail -n 1)
 STAGE3_URL="http://distfiles.gentoo.org/releases/amd64/autobuilds/$STAGE3_FILE"
-wget "$STAGE3_URL" -P /mnt/gentoo/
+wget "$STAGE3_URL" -O "/mnt/gentoo/$STAGE3_FILE"
 tar xpvf "/mnt/gentoo/$STAGE3_FILE" -C /mnt/gentoo --xattrs-include='*.*' --numeric-owner
 rm "/mnt/gentoo/$STAGE3_FILE"
 
@@ -56,65 +56,61 @@ mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
 
-echo "Step 7: Copy DNS information"
-# Copy DNS information
-cp /etc/resolv.conf /mnt/gentoo/etc/resolv.conf
-
-echo "Step 8: Chrooting into the Gentoo environment"
+echo "Step 7: Chrooting into the Gentoo environment"
 # Chroot into the Gentoo environment
-chroot /mnt/gentoo /bin/bash <<EOF
+chroot /mnt/gentoo /bin/bash << "EOF"
 
 source /etc/profile
 export PS1="(chroot) $PS1"
 
-echo "Step 9: Setting the timezone"
+echo "Step 8: Setting the timezone"
 # Set the timezone
 ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
 echo "$TIMEZONE" > /etc/timezone
 
-echo "Step 10: Setting the system clock"
+echo "Step 9: Setting the system clock"
 # Set the system clock
 hwclock --systohc
 
-echo "Step 11: Configuring the locale"
+echo "Step 10: Configuring the locale"
 # Configure the locale
 echo "fi_FI.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 eselect locale set fi_FI.utf8
 
-echo "Step 12: Setting the hostname"
+echo "Step 11: Setting the hostname"
 # Set the hostname
 echo "hostname=\"$HOSTNAME\"" > /etc/conf.d/hostname
 
-echo "Step 13: Setting the root password"
+echo "Step 12: Setting the root password"
 # Set the root password
 echo "root:$ROOT_PASSWORD" | chpasswd
 
-echo "Step 14: Installing the Gentoo base system"
+echo "Step 13: Installing the Gentoo base system"
 # Install the Gentoo base system
 emerge-webrsync
 emerge --sync
 emerge --oneshot sys-apps/portage
 emerge --update --deep --newuse @world
 
-echo "Step 15: Configuring the bootloader (GRUB)"
+echo "Step 14: Configuring the bootloader (GRUB)"
 # Configure the bootloader (GRUB)
 emerge sys-boot/grub:2
 grub-install --target=x86_64-efi --efi-directory="$EFI_MOUNT" --bootloader-id=Gentoo
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Step 16: Enabling necessary services"
+echo "Step 15: Enabling necessary services"
 # Enable necessary services
 rc-update add sshd default
 rc-update add dhcpcd default
 
 EOF
 
-echo "Step 17: Generating fstab"
+echo "Step 16: Generating fstab"
 # Generate fstab
 genfstab -U /mnt/gentoo >> /mnt/gentoo/etc/fstab
 
-echo "Step 18: Unmounting filesystems"
+echo "Step 17: Unmounting filesystems"
 # Unmount filesystems
 umount -R /mnt/gentoo
 umount "$EFI_MOUNT"
