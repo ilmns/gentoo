9dcd0a729ba5f94bd1b2123f8c84ed0e72c35387
ilmns <33120809+ilmns@users.noreply.github.com>
2023-07-27 05:35:59 +0300

    Update install_gentoo.sh

--- install_gentoo.sh
+++ install_gentoo.sh
@@ -60,6 +60,7 @@ prompt_yes_no "Are you sure you want to proceed with the installation?" || exit 
 
 set -e
 
+# Step 1: Partitioning the NVMe drive
 print_status "Step 1: Partitioning the NVMe drive"
 
 # Check if the NVMe drive is already mounted or in use
@@ -81,23 +82,28 @@ fi
 # Update partition table outside the chroot environment
 partprobe "$TARGET_DRIVE" || print_error_and_exit "Failed to update partition table."
 
+# Step 2: Formatting partitions
 print_status "Step 2: Formatting partitions"
+
 # Format the partitions
 mkfs.fat -F 32 "${TARGET_DRIVE}p1" || print_error_and_exit "Failed to format EFI partition."
 mkfs.ext4 "${TARGET_DRIVE}p2" || print_error_and_exit "Failed to format root partition."
 
-
+# Step 3: Mounting the partitions
 print_status "Step 3: Mounting the partitions"
+
 # Mount the partitions
 mount "${TARGET_DRIVE}p2" /mnt/gentoo || print_error_and_exit "Failed to mount root partition."
 mkdir -p "$EFI_MOUNT" || print_error_and_exit "Failed to create EFI mount point."
 mount "${TARGET_DRIVE}p1" "$EFI_MOUNT" || print_error_and_exit "Failed to mount EFI partition."
 
-# Set the date outside the chroot environment
+# Step 4: Setting the date
 print_status "Step 4: Setting the date"
 ntpd -q -g
 
+# Step 5: Downloading the Gentoo stage3 tarball
 print_status "Step 5: Downloading the Gentoo stage3 tarball"
+
 # Download the Gentoo stage3 tarball
 STAGE3_URL="http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64.txt"
 STAGE3_FILE=$(wget -qO- "$STAGE3_URL" | grep -Eo 'stage3-amd64-.*\.tar\.xz' | tail -n 1)
@@ -106,7 +112,9 @@ wget "$STAGE3_URL" -O "/mnt/gentoo/$STAGE3_FILE"
 tar xvf "/mnt/gentoo/$STAGE3_FILE" -C /mnt/gentoo --xattrs-include='*.*' --numeric-owner
 rm "/mnt/gentoo/$STAGE3_FILE"
 
+# Step 6: Configuring the Gentoo installation
 print_status "Step 6: Configuring the Gentoo installation"
+
 # Configure the Gentoo installation
 mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
 mkdir -p /mnt/gentoo/etc/portage/repos.conf
@@ -118,7 +126,9 @@ if [[ -n "$USE_FLAGS" ]]; then
   echo "USE=\"$USE_FLAGS\"" >> /mnt/gentoo/etc/portage/make.conf
 fi
 
+# Step 7: Mounting necessary filesystems
 print_status "Step 7: Mounting necessary filesystems"
+
 # Mount necessary filesystems
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
@@ -126,61 +136,63 @@ mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/dev
 
+# Step 8: Entering the chroot environment
 print_status "Step 8: Entering the chroot environment"
+
 # Entering the chroot environment
 chroot /mnt/gentoo /bin/bash <<EOF
 source /etc/profile
 export PS1="(chroot) \$PS1"
 
+# Step 9: Setting the timezone
 print_status "Step 9: Setting the timezone"
-# Set the timezone
 ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
 echo "$TIMEZONE" > /etc/timezone
 
+# Step 10: Setting the system clock
 print_status "Step 10: Setting the system clock"
-# Set the system clock
 hwclock --systohc
 
+# Step 11: Configuring the locale
 print_status "Step 11: Configuring the locale"
-# Configure the locale
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 eselect locale set en_US.utf8
 
+# Step 12: Setting the hostname
 print_status "Step 12: Setting the hostname"
-# Set the hostname
 echo "hostname=\"$HOSTNAME\"" > /etc/conf.d/hostname
 
+# Step 13: Setting the root password
 print_status "Step 13: Setting the root password"
-# Set the root password
 echo "root:$ROOT_PASSWORD" | chpasswd
 
+# Step 14: Installing the Gentoo base system
 print_status "Step 14: Installing the Gentoo base system"
-# Install the Gentoo base system
 emerge-webrsync
 emerge --sync
 emerge --oneshot sys-apps/portage
 emerge --update --deep --newuse @world
 
+# Step 15: Configuring the bootloader (GRUB)
 print_status "Step 15: Configuring the bootloader (GRUB)"
-# Configure the bootloader (GRUB)
 emerge sys-boot/grub:2
 grub-install --target=x86_64-efi --efi-directory="$EFI_MOUNT" --bootloader-id=Gentoo
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# Step 16: Enabling necessary services
 print_status "Step 16: Enabling necessary services"
-# Enable necessary services
 rc-update add sshd default
 rc-update add dhcpcd default
 
 EOF
 
+# Step 17: Generating fstab
 print_status "Step 17: Generating fstab"
-# Generate fstab
 genfstab -U /mnt/gentoo >> /mnt/gentoo/etc/fstab
 
+# Step 18: Exiting the chroot environment
 print_status "Step 18: Exiting the chroot environment"
-# Exiting the chroot environment
 umount -l /mnt/gentoo/dev{/shm,/pts,}
 umount /mnt/gentoo{/boot/efi,/proc,/sys,}
 umount /mnt/gentoo
