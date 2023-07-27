6e22cda7642310c75fc0162d0885b88aff7bdd69
ilmns <33120809+ilmns@users.noreply.github.com>
2023-07-27 03:59:22 +0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -20,11 +20,30 @@ command -v python3 &>/dev/null || {
   emerge --update --newuse dev-lang/python:3.9
 }
 
+# Check python version
+python_version=$(python3 -c 'import sys; print(sys.version_info.major)')
+if [[ $python_version -eq 3 ]]; then
+  echo "Python 3 is installed"
+else
+  echo "Python 3 is not installed"
+  exit 1
+fi
+
 # Install pip for Python3 if it's not installed
 command -v pip3 &>/dev/null || {
   echo "pip3 not found, installing..."
   emerge --update --newuse dev-python/pip
+  python3 -m ensurepip --upgrade
 }
+
+# Check pip version
+pip_version=$(pip3 -V | awk '{print $2}')
+if [[ $pip_version == *"pip"* ]]; then
+  echo "pip is installed"
+else
+  echo "pip is not installed"
+  exit 1
+fi
 
 # Update pip
 echo "Updating pip..."
